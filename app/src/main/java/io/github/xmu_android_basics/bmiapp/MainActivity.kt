93f4c73adc7d5ec336984b331b2fd6ffe3fc78bc
windwood <windwood@gmail.com>
2019-03-24 11:30:55 +0800

    新建项目 + Android Studio 版本为 3.3.2 (gradle plugin 3.3.2) + 设定项目名称 + 设定 domain 为 xmu-android-basics.github.io + 选择最低运行环境为API 21 + 选择项目模板为 Empty Activity template + 使用 Kotlin 语言 + 使用 AndroidX

---
+++ app/src/main/java/io/github/xmu_android_basics/bmiapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.xmu_android_basics.bmiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/io/github/xmu_android_basics/bmiapp/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package io.github.xmu_android_basics.bmiapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
