5f72269137373a9b3e7a8ebaaf607fdadfaee7e4
windwood <windwood@gmail.com>
2019-03-24 22:13:55 +0800

    给按钮加上响应事件

--- app/src/main/java/io/github/xmu_android_basics/bmiapp/MainActivity.kt
+++ app/src/main/java/io/github/xmu_android_basics/bmiapp/MainActivity.kt
@@ -2,11 +2,24 @@ package io.github.xmu_android_basics.bmiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onComputeClick(view: View) {
+
+    }
+
+    fun onHistoryClick(view: View) {
+
+    }
+
+    fun onShareClick(view: View) {
+
     }
 }
